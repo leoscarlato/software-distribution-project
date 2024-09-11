4acf6c02b00bae5ecc703066b87069eea38b87f0
Leonardo Scarlato <leonardo.scarlato@hotmail.com>
2024-09-11 09:31:38 -0300

    translation

--- pacote_exemplo/scripts/hello.py
+++ pacote_exemplo/scripts/hello.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 from dev_aberto import hello
+from datetime import datetime
+import babel.dates as dates
+import gettext
 
 if __name__ == '__main__':
+    gettext.bindtextdomain('cli', 'locale')
+    gettext.textdomain('cli')
+    _ = gettext.gettext
+
     date, name = hello()
-    print('Último commit feito em:', date, ' por', name)
+
+    real_date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    formatted_date = dates.format_date(real_date, locale='en_US')
+
+    print(_('Último commit feito em:'), formatted_date, _('por'), name)
